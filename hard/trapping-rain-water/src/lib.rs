f21af5bb06a0bdbfd538491554991da5697bd78e
5-pebbles <5-pebble@protonmail.com>
2024-05-08 12:03:09 -0400

    refactor(hard): rm unused import;

--- hard/trapping-rain-water/src/lib.rs
+++ hard/trapping-rain-water/src/lib.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 /// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 ///
 /// # Example
